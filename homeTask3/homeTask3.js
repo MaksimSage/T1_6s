da4057e28c342c871dfbafb135d9455f8c0a1b62
Гибкин Максим Сергеевич (4112843) <mgibkin@vtb.ru>
2025-12-06 11:42:44 +0300

    комментарии

--- homeTask3/homeTask3.js
+++ homeTask3/homeTask3.js
@@ -1,3 +1,5 @@
+
+// определеяем опорный эл-т
 function partition(arr, low, high){
     const pivot = arr[high];
     let i = low - 1;
@@ -5,7 +7,7 @@ function partition(arr, low, high){
     for(let j = low; j < high; j++){
         if(arr[j] <= pivot)  {
             i++
-            [arr[i], arr[j]] = [arr[j], arr[i]]
+            [arr[i], arr[j]] = [arr[j], arr[i]]  //обмен влево если опорный больше
         }
     }
 
@@ -18,17 +20,19 @@ export  function quickSortIterative(arr) {
     if(arr.length <= 1) return arr
 
     const stack = [[0, arr.length - 1]];
-
+//после определения индекса опорноного заполняем стек, разделяя по нему
     while(stack.length > 0){
         const [low, high] = stack.pop()
         
         if(low < high) {
             const pIndex = partition(arr, low, high)
 
+            //кладем правую часть с условием в длину минимум 2
             if(low < pIndex - 1) {
                 stack.push([low, pIndex - 1])
             } 
 
+            //кладем правую часть с условием в длину минимум 2
             if(pIndex + 1 < high) {
                 stack.push([pIndex + 1, high])
             }
